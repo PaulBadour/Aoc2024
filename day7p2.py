688ca013513481ab5911ce59ce138937353dc989
Paul Badour <paulrbadour@gmail.com>
2024-12-07 00:41:22 -0500

    Day 7 (near leaderboard)

---
+++ day7p2.py
@@ -0,0 +1,28 @@
+from aoclib import *
+DAY = 7
+PART = 2
+
+pInp = get_input(DAY)
+ans = 0
+
+# Code
+
+def test(target, nums):
+    if len(nums) == 1:
+        return target == nums[0]
+    
+    n1, n2 = nums.pop(0), nums.pop(0)
+
+    return test(target, [n1 + n2] + nums) or test(target, [n1 * n2] + nums) or test(target, [int(str(n1) + str(n2))] + nums)
+
+for i in pInp:
+    nums = getints(i)
+
+    target = nums.pop(0)
+
+    if test(target, nums):
+        ans += target
+
+    
+
+submit(DAY, PART, ans)
